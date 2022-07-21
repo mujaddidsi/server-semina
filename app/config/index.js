ec2e86fdc87428e4d878f37e99b9c8f928f450aa
mujaddidsi <mujaddid.s.i@hotmail.com>
2022-07-21 21:50:15 +0700

    Create participants collection and mail service

--- app/config/index.js
+++ app/config/index.js
@@ -3,6 +3,8 @@ dotenv.config();
 
 module.exports = {
 	urlDb: process.env.URL_MONGODB_DEV,
-	jwtSecret: process.env.SECRET_KEY,
-	jwtExpiration: '24h',
+	jwtSecret: process.env.JWT_SECRET_KEY,
+	jwtExpiration: process.env.JWT_EXPIRATION,
+	gmail: process.env.EMAIL_GMAIL,
+	password: process.env.PASSWORD_GMAIL,
 };
